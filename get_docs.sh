ec67aa9b83f519978a554cfa8dfd62be5cea6bff
Klemen Tušar <techouse@gmail.com>
2020-07-28 08:36:34 +0100

    :zap: Optimize shell script

--- get_docs.sh
+++ get_docs.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-mkdir docs
-git clone https://github.com/cakephp/docs.git docs_src
-cd docs_src
-git checkout 2.x
-make html-en
-mv build/html/en ../docs/2
-rm -rf build
-git checkout 3.x
-make html-en
-mv build/html/en ../docs/3
-rm -rf build
-git checkout 4.x
-make html-en
-mv build/html/en ../docs/4
-rm -rf build
+
+mkdir -p ./docs
+
+git clone https://github.com/cakephp/docs.git ./docs_src
+
+cd ./docs_src
+
+versions=( 2 3 4 )
+
+for ver in "${versions[@]}"; do
+    git checkout ${ver}.x
+    make html-en
+    mv ./build/html/en ../docs/${ver}
+    rm -rf ./build
+done
+
+rm -rf ./docs_src
